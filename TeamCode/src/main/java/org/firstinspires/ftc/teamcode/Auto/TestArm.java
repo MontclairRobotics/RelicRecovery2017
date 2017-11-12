7b0684795b64ff19e7dc45609cdfb92e239cf36f
MHSRobotics <jack.hymowitz@gmail.com>
2017-11-12 06:20:42 -0500

    previous commit could not build, fixed now. forgot to fix nextState method in TestArm.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestArm.java
@@ -17,12 +17,12 @@ public class TestArm extends DefaultAutoMode {
         switch (state){
             case 0:
                 hardware.jewelArm.setPosition(JEWEL_ARM_DOWN_POS);
-                nextState(pause(3));
+                nextState(pause(3),1);
                 break;
 
             case 1:
                 hardware.jewelArm.setPosition(JEWEL_ARM_UP_POS);
-                nextState(pause(3));
+                nextState(pause(3),2);
                 break;
 
             case 2:
